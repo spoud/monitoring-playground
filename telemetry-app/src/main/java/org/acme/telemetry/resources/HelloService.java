a7f26bd46d8a340081cd31c1739c2ccfbb93f0bd
Michael Stämpfli <michael.staempfli@gmail.com>
2024-02-08 16:08:21 +0100

    Add kafka

--- telemetry-app/src/main/java/org/acme/telemetry/resources/HelloService.java
+++ telemetry-app/src/main/java/org/acme/telemetry/resources/HelloService.java
@@ -3,14 +3,22 @@ package org.acme.telemetry.resources;
 import io.opentelemetry.instrumentation.annotations.SpanAttribute;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.jboss.logging.Logger;
 
 @ApplicationScoped
 public class HelloService {
   private static final Logger LOG = Logger.getLogger(HelloService.class);
 
+  @Inject
+  @Channel("hello")
+  Emitter<String> helloEmitter;
+
   @WithSpan
   public void sayHello(@SpanAttribute String name) {
     LOG.info(String.format("I said hello to %s", name));
+    helloEmitter.send(name);
   }
 }
