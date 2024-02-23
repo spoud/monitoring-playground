64352685bf81b0aa964964ac14780337178a9321
Michael Stämpfli <michael.staempfli@gmail.com>
2024-02-23 16:26:18 +0100

    Add pyroscope

--- telemetry-app/src/main/java/org/acme/telemetry/resources/HelloService.java
+++ telemetry-app/src/main/java/org/acme/telemetry/resources/HelloService.java
@@ -4,6 +4,9 @@ import io.opentelemetry.instrumentation.annotations.SpanAttribute;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.LongStream;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.jboss.logging.Logger;
@@ -16,8 +19,13 @@ public class HelloService {
   @Channel("hello")
   Emitter<String> helloEmitter;
 
+  Random random = new Random();
+
   @WithSpan
   public void sayHello(@SpanAttribute String name) {
+    // simulate computationally expensive operation
+    Arrays.sort(LongStream.rangeClosed(0, 10000000).map((x) -> random.nextLong()).toArray());
+
     LOG.info(String.format("I said hello to %s", name));
     helloEmitter.send(name);
   }
